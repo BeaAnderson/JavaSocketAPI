18de6848bcdc21fb1056f5a90fcb79158ae15345
Bea.Anderson <Bea.Anderson@fdmgroup.com>
2024-03-05 09:15:07 +0000

    added request class

--- src/main/java/com/beacodeart/api/Api.java
+++ src/main/java/com/beacodeart/api/Api.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -13,76 +14,88 @@ import java.util.Map;
 
 public class Api {
 	public static void main(String[] args) throws IOException {
-		//start the server
+		// start the server
 		int port = 8080;
 		ServerSocket serverSocket = new ServerSocket(port);
-		
+
 		System.out.println("Server started on port " + port);
-		
-		//keep the server running
+
+		// keep the server running
 		while (true) {
-			
+
 			Socket client = serverSocket.accept();
 			client.setSoTimeout(0);
-			
+
 			new Thread(() -> {
 				try {
-				InputStream inputStream = client.getInputStream();
-				OutputStream outputStream = client.getOutputStream();
-				
-				String httpRequest = read(inputStream);
-				
-				List<String> lines = Arrays.asList(httpRequest.split("(?m)^\\s*$"));
-				
-				//for (String line: lines) {
-					//System.out.println("line " + line);
-				//}
-				
-				//System.out.println(lines.size());
-				
-				String headers = lines.get(0);
-				//System.out.println(headers);
-				String body;
-				
-				if (lines.size()>1) {
-					body = lines.get(1);
-					//System.out.println(body);
-				}
-				
-				//hello world response
-				OutputStream clientOutput = client.getOutputStream();
-				clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
-				clientOutput.write(("\r\n").getBytes());
-				clientOutput.write(("Hello World").getBytes());
-				clientOutput.flush();
-				
-				
+					InputStream inputStream = client.getInputStream();
+					OutputStream outputStream = client.getOutputStream();
+
+					String httpRequest = read(inputStream);
+
+					List<String> lines = Arrays.asList(httpRequest.split("(?m)^\\s*$"));
+
+					// for (String line: lines) {
+					// System.out.println("line " + line);
+					// }
+
+					// System.out.println(lines.size());
+
+					String headers = lines.get(0);
+					// System.out.println(headers);
+					String body;
+
+					if (lines.size() > 1) {
+						body = lines.get(1);
+						// System.out.println(body);
+					}
+
+					// hello world response
+					OutputStream clientOutput = client.getOutputStream();
+					clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+					clientOutput.write(("\r\n").getBytes());
+					clientOutput.write(("Hello World").getBytes());
+					clientOutput.flush();
+
 				} catch (IOException ex) {
 					ex.printStackTrace();
 				} finally {
 					try {
 						client.close();
-					} catch (IOException e){
+					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}).start();
-				
-				
-			
-			
+
 		}
 	}
-			
-	
+
 	static String read(InputStream input) throws IOException {
 		StringBuilder result = new StringBuilder();
-		
+
 		do {
 			result.append((char) input.read());
-		} while (input.available()>0);
-		
+		} while (input.available() > 0);
+
 		return result.toString();
 	}
+
+	static class Request {
+		private Map<String, String> headers;
+		private String body;
+
+		public Request(Map<String, String> headers, String body) {
+			this.headers = headers;
+			this.body = body;
+		}
+
+		public String getBody() {
+			return body;
+		}
+
+		public Map<String, String> getHeaders() {
+			return headers;
+		}
+	}
 }
-	
